6d66fd800f0fc59611e6b3bac56f40e87de760be
Daiki Tanaka <daiki.yosky@gmail.com>
2019-09-11 20:21:11 +0900

    fix final convolutional layer

--- model.py
+++ model.py
@@ -20,6 +20,7 @@ class GatedRecurrentConvLayer(nn.Module):
         self.conv_recurrent_gated = nn.Conv2d(out_channels, out_channels,
                                               kernel_size=(1,1), padding=0, stride=(1,1))
 
+
         self.conv_forward = nn.Sequential(nn.Conv2d(in_channels, out_channels,
                                                     kernel_size=kernel_size, padding=padding, stride=stride),
                                           nn.BatchNorm2d(out_channels),
@@ -44,6 +45,8 @@ class GatedRecurrentConvLayer(nn.Module):
         x_t = F.relu(forward_conv)
         gate_forward_conv = self.conv_forward_gated(x)
 
+        assert x_t.size() == gate_forward_conv.size()
+
         # t > 0
         for timestep in range(self.n_iter):
             last_x = x_t
@@ -61,65 +64,72 @@ class GatedRecurrentConvLayer(nn.Module):
 
 
 class GatedRecurrentConvNet(nn.Module):
-    # This network was proposed in :
+    # This network is proposed in :
     # http://papers.nips.cc/paper/6637-gated-recurrent-convolution-neural-network-for-ocr.pdf
 
     def __init__(self, nclasses, in_channels=3, img_size=(64, 256)):
         super(GatedRecurrentConvNet, self).__init__()
 
-        """
-        Args :
-            nclasses : The number of characters to output
-            in_channels : The number of channels of input images
-            img_size : The size of input images (Height, Width)
-        """
-
         self.nclasses = nclasses
         self.in_channels = in_channels
 
+        channel_num = [in_channels, 32, 64, 128, 256, 256]
+
         self.conv1 = nn.Sequential(
-                                    nn.Conv2d(in_channels, out_channels=64,
+                                    nn.Conv2d(channel_num[0], out_channels=channel_num[1],
                                               kernel_size=(3,3), stride=1, padding=1),
+                                    nn.BatchNorm2d(channel_num[1]),
+                                    nn.ReLU(),
                                     nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))
-                                    )
+        )
         self.GRCL1 = nn.Sequential(
-                                    GatedRecurrentConvLayer(in_channels=64,
-                                                             out_channels=64,
+                                    GatedRecurrentConvLayer(in_channels=channel_num[1],
+                                                             out_channels=channel_num[2],
                                                              kernel_size=(3,3),
                                                              stride=(1,1),
                                                              padding=(1,1),
+                                                             n_iter=3
                                                              ),
                                     nn.MaxPool2d(kernel_size=(2,2),stride=(2,2))
                                     )
         self.GRCL2 = nn.Sequential(
-                                    GatedRecurrentConvLayer(in_channels=64,
-                                                             out_channels=128,
+                                    GatedRecurrentConvLayer(in_channels=channel_num[2],
+                                                             out_channels=channel_num[3],
                                                              kernel_size=(3,3),
                                                              stride=(1,1),
                                                              padding=(1,1),
+                                                             n_iter=3,
                                                              ),
                                     nn.MaxPool2d(kernel_size=(2,2),stride=(2,1), padding=(0,1))
                                     )
+
         self.GRCL3 = nn.Sequential(
-                                    GatedRecurrentConvLayer(in_channels=128,
-                                                             out_channels=256,
+                                    GatedRecurrentConvLayer(in_channels=channel_num[3],
+                                                             out_channels=channel_num[4],
                                                              kernel_size=(3,3),
                                                              stride=(1,1),
                                                              padding=(1,1),
+                                                             n_iter=3
                                                              ),
                                     nn.MaxPool2d(kernel_size=(2,2),stride=(2,1), padding=(0,1))
                                     )
 
         if img_size[0] == 32:
-            self.conv2 = nn.Conv2d(in_channels=256, out_channels=512,
-                                    kernel_size=(2,2), stride=(1,1))
+            self.conv2 = nn.Sequential(nn.Conv2d(in_channels=channel_num[4], out_channels=channel_num[5],
+                                                kernel_size=(2,2), stride=(1,1)),
+                                       nn.BatchNorm2d(channel_num[5]),
+                                       )
         elif img_size[0] == 64:
-            self.conv2 = nn.Sequential(nn.Conv2d(in_channels=256, out_channels=512,
+            self.conv2 = nn.Sequential(nn.Conv2d(in_channels=channel_num[4], out_channels=channel_num[5],
                                                     kernel_size=(2,2), stride=(1,1)),
-                                       nn.MaxPool2d(kernel_size=(2,1), stride=(2,1))
+                                       nn.BatchNorm2d(channel_num[5], momentum=.5),
+                                       nn.ReLU(),
+                                       nn.Conv2d(in_channels=channel_num[5], out_channels=channel_num[5],
+                                                kernel_size=(2,2), stride=(2,1)),
+                                       nn.BatchNorm2d(channel_num[5], momentum=.5),
                                        )
 
-        rnn_in = 512
+        rnn_in = 256
         hidden = 256
         num_layers = 2
         self.lstm = nn.LSTM(input_size=rnn_in,
@@ -131,7 +141,6 @@ class GatedRecurrentConvNet(nn.Module):
         self.fc = nn.Linear(hidden*2, nclasses)
 
     def forward(self, x):
-
         x = self.conv1(x)
         x = self.GRCL1(x)
         x = self.GRCL2(x)
@@ -139,11 +148,12 @@ class GatedRecurrentConvNet(nn.Module):
         x = self.conv2(x)
 
         batchsize, channels, height, width = x.size()
+
         assert height == 1, "Height must be 1, but get {}".format(height)
 
         x = x.squeeze(2)
-        x = x.permute(0, 2, 1)  # (batch_size, seq_length, features)
-        x, _ = self.lstm(x)
+        x = x.permute(0, 2, 1)  # batch_size, seq_length, feat
+        x = self.lstm(x)[0]
         out = self.fc(x)
 
         return out
